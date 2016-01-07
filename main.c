5f93bb51a32a34ed3d9ab49f1bbeb88f30f99d81
Julien Barbier <write0@gmail.com>
2016-01-06 20:25:32 -0800

    typos

--- main.c
+++ main.c
@@ -7,7 +7,7 @@
  *  \ V  V / | |  _| | |  __/ (_| \__ \__ \\ V  V / (_) | | | (_| |
  *   \_/\_/  |_|_| |_| |_|   \__,_|___/___/ \_/\_/ \___/|_|  \__,_|
  *
- * Acces point: Holberton Guest
+ * SSID: Holberton School Guest
  *
  * https://www.holbertonschool.com
  *
